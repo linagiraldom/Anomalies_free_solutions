21ff5bc44b2f3445a035760114230025ad42a20f
Lina Marcela Giraldo-Murillo <50031393+linagiraldom@users.noreply.github.com>
2022-09-29 13:10:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
         name="Anomalies_free_solutions",
 
         # Version number (initial):
-        version="0.0.1",
+        version="0.0.2",
 
         # Application author details:
         author="linagiraldom",
